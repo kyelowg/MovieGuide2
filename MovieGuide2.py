5e0e7677926793e5f1f324dde975e8c7f2e99a81
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-13 10:58:42 -0800

    MovieGuide2Final

--- MovieGuide2.py
+++ MovieGuide2.py
@@ -1,28 +1,29 @@
-FILENAME = "movies.txt"
+#Kye Lowmon CIS261 MovieGuide2 week6
+FILENAME = "movies.txt.txt"
 
 def write_movies(movies):
     with open(FILENAME, "w")as file:
-     for movie in movies:
-         file.write(f"{movies}\n")
+        for movie in movies:
+            file.write(f"{movies}\n")
 
 def read_movies():
     movies = []
-    with open (FILENAME) as file:
+    with open(FILENAME) as file:
         for line in file:
-            line = line.replace("\n","")
+            line = line.replace("\n", "")
             movies.append(line)
     return movies
 
 def list_movies(movies):
     for i, movie in enumerate(movies, start=1):
         print(f"{i}. {movie}")
-        print()
+    print()
 
 def add_movie(movies):
     movie = input("Movies: ")
     movies.append(movie)
     write_movies(movies)
-    print(f"{mpvie} was added. \n")
+    print(f"{movie} was added. \n")
 
 def delete_movie(movies):
     index = int(input("Number: "))
@@ -34,7 +35,31 @@ def delete_movie(movies):
         print(f"{movie} was deleted. \n")
 
 def display_menu():
-    print("The Movie List Program ")
+    print("The Movie List program ")
     print()
     print("COMMAND MENU")
-    print('list - list all movies')
+    print("list - List all movies")
+    print("add - Add a movie")
+    print("del - Delete a movie")
+    print("exit - Exit program")
+    print()
+
+def main():
+    display_menu()
+    movies = read_movies()
+    while True:
+        command = input("Command: ")
+        if command.lower() == "list":
+            list_movies(movies)
+        elif command.lower() == "add":
+            add_movie(movies)
+        elif command.lower() == "del":
+            delete_movie(movies)
+        elif command.lower() == "exit":
+            print("Bye!")
+            break
+        else:
+            print("Not a valid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
